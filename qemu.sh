a2ad201dd43d40ae088e689aae34055e82b2f07e
Joachim Nilsson <troglobit@gmail.com>
2018-03-11 23:30:49 +0100

    qemu.sh: Enable RTC emulation, add smc91c111 NIC
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- qemu.sh
+++ qemu.sh
@@ -3,9 +3,8 @@
 # Disable pulseaudio warning
 export QEMU_AUDIO_DRV=none
 
-qemu-system-arm -m 256M -M versatilepb -nographic \
-		-kernel images/zImage \
-		-append 'root=/dev/rom0 console=ttyAMA0 mem=256M' \
-		-dtb images/versatile-pb.dtb \
-		-initrd images/tinyroot.cpio.gz
-		
+qemu-system-arm -m 256M -M versatilepb -rtc base=utc,clock=rt -nographic \
+		-net bridge,br=virbr0 -net nic,model=smc91c111           \
+		-kernel images/zImage  -dtb images/versatile-pb.dtb      \
+		-initrd images/tinyroot.cpio.gz                          \
+		-append 'root=/dev/rom0 console=ttyAMA0 mem=256M'
