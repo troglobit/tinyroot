9bb7f7d7f99bde29fafa9f092cd588decda1d8bd
Joachim Nilsson <troglobit@gmail.com>
2018-03-12 02:36:07 +0100

    qemu: Change to versatileab, boot SquashFS image and silence kernel
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- qemu.sh
+++ qemu.sh
@@ -3,8 +3,9 @@
 # Disable pulseaudio warning
 export QEMU_AUDIO_DRV=none
 
-qemu-system-arm -m 256M -M versatilepb -rtc base=utc,clock=rt -nographic \
+# -initrd images/tinyroot.cpio.gz + root=/dev/rom0
+qemu-system-arm -m 256M -M versatileab -rtc base=utc,clock=rt -nographic \
 		-net bridge,br=virbr0 -net nic,model=smc91c111           \
-		-kernel images/zImage  -dtb images/versatile-pb.dtb      \
-		-initrd images/tinyroot.cpio.gz                          \
-		-append 'root=/dev/rom0 console=ttyAMA0 mem=256M'
+		-kernel images/zImage  -dtb images/versatile-ab.dtb      \
+		-initrd images/tinyroot.img                              \
+		-append 'root=/dev/ram console=ttyAMA0 mem=256M quiet splash'
